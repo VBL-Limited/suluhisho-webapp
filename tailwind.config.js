642b03ad5d8c24c15fbf9525aa939d026d33a081
brackleycasinga <pcassinga@gmail.com>
2022-04-09 13:06:54 +0200

    Fixed deployemnt error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,30 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}','./public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-        fontFamily: {
-            body:['Montserrat'],
-            text:['IBM Plex Sans Thai Looped', 'sans-serif'],
-           bode2: ['Montserrat', 'sans-serif;']
-        },
+      fontFamily: {
+        body: ["Montserrat"],
+        text: ["IBM Plex Sans Thai Looped", "sans-serif"],
+        bode2: ["Montserrat", "sans-serif;"],
+      },
+      colors: {
+        transparent: "transparent",
+        primary: "#008FDD",
+        current: "currentColor",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        bermuda: "#78dcca",
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
