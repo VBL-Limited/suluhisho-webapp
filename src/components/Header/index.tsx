e0ffcb3a5e01e2b509b0c97b53048421fee1bc03
brackleycasinga <pcassinga@gmail.com>
2022-04-18 04:06:14 +0200

    Remove image to test link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Logo from "../../img/Logo.png";
+// import Logo from "../../img/Logo.png";
 const Header = () => {
   return (
     <>
