ce8bfd0b1f253534c3b08b9d4d47122694fa46f9
brackleycasinga <pcassinga@gmail.com>
2022-04-20 22:56:28 +0200

    Add job card design to home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,59 +1,59 @@
-import { Hero, JobCard, Notification } from "../../components";
-import { useEffect, useState } from "react";
-import { getToken, getUser } from "../../api/User";
-import axios from "axios";
+import { Hero, JobCard } from "../../components";
+// import { useEffect, useState } from "react";
+// import { getToken, getUser } from "../../api/User";
+// import axios from "axios";
 const Home = () => {
-  const [userInfo, setUserInfos] = useState();
-  const token = getToken();
-  const user = getUser();
-  const [jobs, setJobs] = useState<any>();
-  const [isLogedin, setIsloggedin] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [statusMessage, setStatusMessage] = useState("");
-  const [status, setStatus] = useState<any>();
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get(`${process.env.BASE_URL}/api/offre?page=1&limit=3`)
-      .then((res) => {
-        const offres = res.data;
-        if (offres.results) {
-          setLoading(false);
-          setJobs(offres?.results);
-        }
-      });
-  }, [token]);
-  useEffect(() => {
-    if (user) {
-      setUserInfos(user.userId);
-    }
-  }, [user]);
-  const handleClose = (
-    event?: React.SyntheticEvent | Event,
-    reason?: string
-  ) => {
-    if (reason === "clickaway") {
-      return;
-    }
+  // const [userInfo, setUserInfos] = useState();
+  // const token = getToken();
+  // const user = getUser();
+  // const [jobs, setJobs] = useState<any>();
+  // const [isLogedin, setIsloggedin] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  // const [statusMessage, setStatusMessage] = useState("");
+  // const [status, setStatus] = useState<any>();
+  // useEffect(() => {
+  //   setLoading(true);
+  //   axios
+  //     .get(`${process.env.BASE_URL}/api/offre?page=1&limit=3`)
+  //     .then((res) => {
+  //       const offres = res.data;
+  //       if (offres.results) {
+  //         setLoading(false);
+  //         setJobs(offres?.results);
+  //       }
+  //     });
+  // }, [token]);
+  // useEffect(() => {
+  //   if (user) {
+  //     setUserInfos(user.userId);
+  //   }
+  // }, [user]);
+  // const handleClose = (
+  //   event?: React.SyntheticEvent | Event,
+  //   reason?: string
+  // ) => {
+  //   if (reason === "clickaway") {
+  //     return;
+  //   }
 
-    setIsloggedin(false);
-  };
+  //   setIsloggedin(false);
+  // };
 
-  const handleAppy = (offreId: any) => {
-    if (userInfo) {
-      setIsloggedin(true);
-      setStatusMessage("Vous avez postulé avec succès à ce poste");
-      setStatus("success");
-    } else {
-      setIsloggedin(true);
-      setStatus("error");
-      setStatusMessage("Vous devez vous connecter pour postuler");
-    }
-    console.log("OffereId", offreId);
-    console.log("userId", userInfo);
-    setIsloggedin(true);
-  };
-  console.log(jobs);
+  // const handleAppy = (offreId: any) => {
+  //   if (userInfo) {
+  //     setIsloggedin(true);
+  //     setStatusMessage("Vous avez postulé avec succès à ce poste");
+  //     setStatus("success");
+  //   } else {
+  //     setIsloggedin(true);
+  //     setStatus("error");
+  //     setStatusMessage("Vous devez vous connecter pour postuler");
+  //   }
+  //   console.log("OffereId", offreId);
+  //   console.log("userId", userInfo);
+  //   setIsloggedin(true);
+  // };
+  // console.log(jobs);
   return (
     <>
       <Hero />
--- src/components/JobCard/index.tsx
+++ src/components/JobCard/index.tsx
@@ -1,11 +1,7 @@
-import job from "../../img/job.jpeg";
-import { Jobstype } from "../../types";
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Grid";
